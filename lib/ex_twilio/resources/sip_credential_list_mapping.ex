a3e6d226be99b22e85bcbac35c088d06a61a761e
Grey <robert-grey.keith@podium.com>
2024-10-25 15:21:56 -0700

    add sip credential list mapping

---
+++ lib/ex_twilio/resources/sip_credential_list_mapping.ex
@@ -0,0 +1,28 @@
+defmodule ExTwilio.SipCredentialListMapping do
+  @moduledoc """
+  Represents a SIP Credential List Mapping resource in the Twilio API.
+
+  - [Twilio docs](https://www.twilio.com/docs/voice/sip/api/sip-credentiallistmapping-resource)
+  """
+
+  defstruct sid: nil,
+            account_sid: nil,
+            friendly_name: nil,
+            date_created: nil,
+            date_updated: nil,
+            uri: nil
+
+  use ExTwilio.Resource,
+    import: [
+      :stream,
+      :all,
+      :find,
+      :create,
+      :update,
+      :destroy
+    ]
+
+  def resource_name, do: "SIP/CredentialListMappings"
+  def resource_collection_name, do: "credential_list_mappings"
+  def parents, do: [:account, :sip_domain]
+end
